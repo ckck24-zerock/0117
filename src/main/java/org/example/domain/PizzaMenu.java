93baf3f5916610ce68f99740907f75d80e7600d3
zerock <kangckck24@gmail.com>
2025-01-17 13:46:46 +0900

    www

--- src/main/java/org/example/domain/PizzaMenu.java
+++ src/main/java/org/example/domain/PizzaMenu.java
@@ -1,4 +1,17 @@
 package org.example.domain;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public class PizzaMenu extends Menu{
+
+    private PizzaSize size;
+
+    public PizzaMenu(int mno, String name, int price, PizzaSize size) {
+        super(mno, name, price);
+        this.size = size;
+    }
+
 }
